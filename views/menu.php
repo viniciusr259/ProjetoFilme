47fbfe5d516d91989fb52f79b7a05f1b6e96d9a4
Vinicius <viniciosr259@gmail.com>
2018-03-30 17:28:49 -0300

    $
    $

--- views/menu.php
+++ views/menu.php
@@ -1,3 +1,16 @@
+<link rel="stylesheet" type="text/css" href="../css/style_menu.css">
 <div id="SeguraMenu">
+  <div id="Parte1">
 
+  </div>
+  <div id="Parte2">
+    <div id="TituloPag">
+      <img src="../imagens/Logo.png" alt="HOMEPAGE">
+    </div>
+  </div>
+  <div id="Parte3">
+    <div id="Login">
+      <img src="../imagens/user.png" alt="Logar">
+    </div>
+  </div>
 </div>
